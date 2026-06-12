180928738f82d582471034376e4de781d6dc8ac9
Yuuki Nishiyama <yuukin@iis.u-tokyo.ac.jp>
2026-06-11 18:21:13 -0700

    Rename DB table to ios_calendar
    - Rename databaseTableName from CalendarData to ios_calendar

--- Sources/com.awareframework.ios.sensor.calendar/model/CalendarData.swift
+++ Sources/com.awareframework.ios.sensor.calendar/model/CalendarData.swift
@@ -3,7 +3,7 @@ import GRDB
 import com_awareframework_ios_core
 
 public struct CalendarData: BaseDbModelSQLite {
-    public static let databaseTableName = "calendarData"
+    public static let databaseTableName = "ios_calendar"
     public static let TABLE_NAME = databaseTableName
 
     public var id: Int64?
